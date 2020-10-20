c153ff90db75b167604fe1db8cd718da1010791a
SXQ_Z <1156868264@qq.com>
2020-10-20 18:38:13 +0800

    @ZJY

--- src/LCJava/LC1567GetMaxLen.java
+++ src/LCJava/LC1567GetMaxLen.java
@@ -42,7 +42,71 @@ package LCJava;
  * 提示：
  * 1 <= nums.length <= 10^5
  * -10^9 <= nums[i] <= 10^9
-
+ *
+ * 思路： 动态规划  定义两个数组 positive表示得到乘积为正数的最长子数组长度，negative表示得到乘积为负数的最长子数组长度。
+ *       当i = 0时，num[i] > 0,则为正数，positive[0]初始化为1，当num[i] < 0,则为负数，negative[0]初始化为1，当为0的时候，
+ *       positive[0] = negative[0] = 0，表示没有这样的子数组
+ *       i > 0 时
+ *        当num[i] > 0,并不会更改之前乘积的正负性，所以
+ *         positive[i] = positive[i−1]+1
+ *         negative[i] = negative[i - 1] > 0 ? negative[i - 1] + 1 : 0;
+ *        当num[i] < 0,会更改之前乘积的正负性，所以
+ *         positive[i] = negative[i - 1] > 0 ? negative[i - 1] + 1 : 0
+ *         negative[i] = positive[i−1]+1;
+ *        当num[i] = 0,结果一定为0，所以 positive[i] = negative[i] = 0
+ * ​
  */
 public class LC1567GetMaxLen {
+
+    public static int getMaxLen(int[] nums) {
+        int length = nums.length;
+        int positive = nums[0] > 0 ? 1 : 0;
+        int negative = nums[0] < 0 ? 1 : 0;
+        int maxLength = positive;
+        for (int i = 1; i < length; i++) {
+            if (nums[i] > 0) {
+                positive++;
+                negative = negative > 0 ? negative + 1 : 0;
+            } else if (nums[i] < 0) {
+                int newPositive = negative > 0 ? negative + 1 : 0;
+                int newNegative = positive + 1;
+                positive = newPositive;
+                negative = newNegative;
+            } else {
+                positive = 0;
+                negative = 0;
+            }
+            maxLength = Math.max(maxLength, positive);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,5,-6,4,0,10};
+        System.out.println(getMaxLen(nums));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
